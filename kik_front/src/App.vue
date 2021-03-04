3d388a12ad84bec3e93a8128f1751b4556feaf19
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2021-03-04 22:00:40 +0100

    create board and function onclick

--- kik_front/src/App.vue
+++ kik_front/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TicTacToe />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TicTacToe from "./components/TicTacToe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TicTacToe,
+  },
+};
 </script>
 
 <style>
---
+++ kik_front/src/components/TicTacToe.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="board-main">
+    <div>
+      <div class="board-item" id="a1" v-on:click="clicked">1</div>
+      <div class="board-item" id="a2" v-on:click="clicked">2</div>
+      <div class="board-item" id="a3" v-on:click="clicked">3</div>
+    </div>
+    <div>
+      <div class="board-item" id="b1" v-on:click="clicked">4</div>
+      <div class="board-item" id="b2" v-on:click="clicked">5</div>
+      <div class="board-item" id="b3" v-on:click="clicked">6</div>
+    </div>
+    <div>
+      <div class="board-item" id="c1" v-on:click="clicked">7</div>
+      <div class="board-item" id="c2" v-on:click="clicked">8</div>
+      <div class="board-item" id="c3" v-on:click="clicked">9</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    clicked: function(event) {
+      console.log(event.target);
+    },
+  },
+};
+</script>
+<style scoped>
+.board-main {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 600px;
+  display: flex;
+}
+.board-item {
+  border: 2px solid black;
+  width: 200px;
+  height: 200px;
+}
+</style>
